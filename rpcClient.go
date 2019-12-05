3288a6953de32430c1ec06ea1c87dacde432f6b6
zhangbobo131415 <821806480@qq.com>
2019-12-05 19:55:35 +0800

    cuo wu xiu zheng

--- rpcClient.go
+++ rpcClient.go
@@ -9,10 +9,9 @@ import (
 	"github.com/apache/thrift/lib/go/thrift"
 )
 
-
 func main() {
 	netAddres := "172.21.13.121:8000"
-	defalt_context :=context.Background()
+	defalt_context := context.Background()
 	transportFactory := thrift.NewTFramedTransportFactory(thrift.NewTTransportFactory())
 	protocolfactory := thrift.NewTBinaryProtocolFactoryDefault()
 	transport, err := thrift.NewTSocket(netAddres)
@@ -20,16 +19,16 @@ func main() {
 		fmt.Println("err", err)
 		os.Exit(1)
 	}
-	useTransport,_ := transportFactory.GetTransport(transport)
-	client:=sayhello.NewHelloAndSumClientFactory(useTransport, protocolfactory)
+	useTransport, _ := transportFactory.GetTransport(transport)
+	client := sayhello.NewHelloAndSumClientFactory(useTransport, protocolfactory)
 	if err := transport.Open(); err != nil {
-        fmt.Fprintln(os.Stderr, "Error opening socket to 127.0.0.1:19090", " ", err)
-        os.Exit(1)
+		fmt.Fprintln(os.Stderr, "Error opening socket to 172.21.13.121:8000", " ", err)
+		os.Exit(1)
 	}
 	defer transport.Close()
 	var i int32
-	for i =0;i<200;i++{
-		tmp,_:=client.Sum(defalt_context,i,i)
+	for i = 0; i < 200; i++ {
+		tmp, _ := client.Sum(defalt_context, i, i)
 		fmt.Println(tmp)
 
 	}
